f2eb9323608fa1d7591e15e0db4fec95fc8cfb00
flex <1851760454@qq.com>
2026-01-14 17:49:02 +0800

    字符串正则工具函数提交

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+    // let count = 0, result;
+    // // console.log(newStr);
+    // result = regular.exec(str);
+    // while (result) {
+    //     ++count;
+    //     regular.exec(str);
+    // }
